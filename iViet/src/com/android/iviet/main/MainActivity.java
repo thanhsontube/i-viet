719c39afd8bc48c6d9996c20fd6213e3d24a77f3
nguyenthanhson.uit@gmail.com <nguyenthanhson.uit@gmail.com@0fa0d82a-09b0-fb12-e276-0d0e7977c2d0>
2014-05-15 12:16:18 +0000

    drawer_1

--- iViet/src/com/android/iviet/main/MainActivity.java
+++ iViet/src/com/android/iviet/main/MainActivity.java
@@ -3,10 +3,15 @@ package com.android.iviet.main;
 import android.app.ActionBar;
 import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
+import com.android.iviet.IVietApplication;
 import com.android.iviet.R;
 import com.android.iviet.base.BaseFragmentActivity;
 import com.android.iviet.main.dto.MainDto;
@@ -19,6 +24,9 @@ import com.android.iviet.utils.FilterLog;
 public class MainActivity extends BaseFragmentActivity implements ITop1FragmentListener, IMainFragmentListener{
 	
 	private static final String TAG = "MainActivity";
+	protected DrawerLayout mDrawerLayout;
+	protected ListView mDrawerList;
+	private ActionBarDrawerToggle mDrawerToggle;
 	FilterLog log = new FilterLog(TAG);
 
 	@Override
@@ -36,15 +44,39 @@ public class MainActivity extends BaseFragmentActivity implements ITop1FragmentL
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main_activity);
+		
+		
+		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+		
+		mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.drawable.ic_drawer, R.string.user_info, 0) {
+			public void onDrawerOpened(View drawerView) {
+			}
+		};
+		mDrawerLayout.setDrawerListener(mDrawerToggle);
+		
+		mDrawerList = (ListView) findViewById(R.id.left_drawer);
+		mDrawerList.setAdapter(getDrawerAdapter());
+		mDrawerList.setOnItemClickListener(null);
+
 		ActionBar actionBar = getActionBar();
-		actionBar.setDisplayHomeAsUpEnabled(false);
+		actionBar.setDisplayHomeAsUpEnabled(true);
+		actionBar.setHomeButtonEnabled(true);
 		actionBar.setDisplayShowCustomEnabled(true);
 		actionBar.setDisplayShowTitleEnabled(false);
-
 		LayoutInflater inflator = (LayoutInflater) this .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		View v = inflator.inflate(R.layout.actionbar_custom, null);
-
 		actionBar.setCustomView(v);
+	}
+	
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+	    super.onPostCreate(savedInstanceState);
+	    mDrawerToggle.syncState();
+	}
+	
+	@Override
+	protected void onResume() {
+	    super.onResume();
 	}
 
 	@Override
@@ -65,5 +97,10 @@ public class MainActivity extends BaseFragmentActivity implements ITop1FragmentL
 		log.d("NECVN>>>" + "onTop1ContentClicked");
 	    
     }
+	
+	protected ListAdapter getDrawerAdapter() {
+		final IVietApplication app = (IVietApplication) getApplication();
+		return null;
+	}
 
 }
