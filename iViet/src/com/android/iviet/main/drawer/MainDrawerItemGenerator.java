55ebb1a3f5c4398332272cd8703f2c45a0cfbe0b
nguyenthanhson.uit@gmail.com <nguyenthanhson.uit@gmail.com@0fa0d82a-09b0-fb12-e276-0d0e7977c2d0>
2014-05-19 05:03:58 +0000

    add test2f

--- iViet/src/com/android/iviet/main/drawer/MainDrawerItemGenerator.java
+++ iViet/src/com/android/iviet/main/drawer/MainDrawerItemGenerator.java
@@ -6,6 +6,7 @@ import java.util.List;
 import com.android.iviet.R;
 import com.android.iviet.main.fragment.MainFragment;
 import com.android.iviet.test.Test1Fragment;
+import com.android.iviet.test.Test2Fragment;
 
 import android.content.Context;
 
@@ -23,7 +24,7 @@ public class MainDrawerItemGenerator implements DrawerItemGenerator<com.android.
 		list.add(new FragmentProfileDrawerItem(0, context.getText(R.string.home), "profile", "profile", null));
 		list.add(new FragmentChangeDrawerItem(0, context.getText(R.string.home), "home", "home", new MainFragment()));
 		list.add(new FragmentChangeDrawerItem(0, context.getText(R.string.notification), "notification", "notification", new Test1Fragment()));
-		list.add(new FragmentChangeDrawerItem(0, context.getText(R.string.help), "help", "help", null));
+		list.add(new FragmentChangeDrawerItem(0, context.getText(R.string.help), "help", "help", new Test2Fragment()));
 		list.add(new FragmentChangeDrawerItem(0, context.getText(R.string.about), "about", "about", null));
 		list.add(new FragmentChangeDrawerItem(0, context.getText(R.string.search), "search", "search", null));
 		list.add(new FragmentChangeDrawerItem(0, context.getText(R.string.logout), "logout", "logout", null));
